bf54bcfe00901186967b69f5341795a58914035c
Michał <michalczeczek@gmail.com>
2018-03-05 16:15:48 +0100

    Added string resources and possibility to set locale in config

--- UI/MyBudget.UI/App.xaml.cs
+++ UI/MyBudget.UI/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace MyBudget.UI
 {
@@ -13,5 +16,22 @@ namespace MyBudget.UI
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var localeFromConfig = ConfigurationManager.AppSettings["locale"]?.ToString();
+
+            if (!string.IsNullOrEmpty(localeFromConfig))
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(localeFromConfig); ;
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(localeFromConfig); ;
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
