8c0530f406657cc63d94bd16eee64bfe0ea0de2a
Michał <michalczeczek@gmail.com>
2018-03-10 20:06:13 +0100

    Fixed maximzedwindow size. Fixed behavior when maximized minimized and then restore from minimized.

---
+++ UI/MyBudget.UI/Common/Behaviors/TitleBarButtonsBehavior.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyBudget.UI.Behaviors
+{
+    public enum WindowBehavior { Maximize, Minimize, Close }
+
+    internal static class WindowExtensions
+    {
+        public static void SetState(this MainWindow window, WindowBehavior behavior)
+        {
+            if (behavior == WindowBehavior.Close)
+                return;
+
+            window.WindowState = behavior == WindowBehavior.Maximize
+                ? WindowState.Maximized
+                : WindowState.Minimized;
+        }
+    }
+
+    public class TitleBarButtonsBehavior : BehaviorBase<Button>
+    {
+        public static readonly DependencyProperty WindowBehaviorProperty = DependencyProperty.Register(
+            "WindowBehavior", typeof(WindowBehavior), typeof(TitleBarButtonsBehavior), new FrameworkPropertyMetadata(WindowBehavior.Close));
+
+        public WindowBehavior WindowBehavior
+        {
+            get { return (WindowBehavior)GetValue(WindowBehaviorProperty); }
+            set { SetValue(WindowBehaviorProperty, value); }
+        }
+
+        protected override void OnSetup()
+        {
+            AssociatedObject.Click += AssociatedObject_Click; ;
+        }
+
+        private void AssociatedObject_Click(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.Command?.Execute(AssociatedObject.CommandParameter);
+            var window = Window.GetWindow(AssociatedObject) as MainWindow;
+            var behavior = WindowBehavior;
+            var state = window.WindowState;
+
+            if (behavior == WindowBehavior.Close)
+            {
+                window.Close();
+                return;
+            }
+
+            if (state == WindowState.Normal)
+            {
+                window.SetState(behavior);
+            }
+            else // only maximized possible
+            {
+                if (behavior == WindowBehavior.Maximize)
+                    window.WindowState = WindowState.Normal;
+                else
+                    window.WindowState = WindowState.Minimized;
+            }
+        }
+
+        protected override void OnCleanup()
+        {
+            AssociatedObject.Click -= AssociatedObject_Click;
+        }
+    }
+}
--- UI/MyBudget.UI/MainWindow.xaml.cs
+++ UI/MyBudget.UI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,21 +9,291 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace MyBudget.UI
 {
+    internal enum AccentState
+    {
+        ACCENT_DISABLED = 0,
+        ACCENT_ENABLE_GRADIENT = 1,
+        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+        ACCENT_ENABLE_BLURBEHIND = 3,
+        ACCENT_INVALID_STATE = 4
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct AccentPolicy
+    {
+        public AccentState AccentState;
+        public int AccentFlags;
+        public int GradientColor;
+        public int AnimationId;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct WindowCompositionAttributeData
+    {
+        public WindowCompositionAttribute Attribute;
+        public IntPtr Data;
+        public int SizeOfData;
+    }
+
+    internal enum WindowCompositionAttribute
+    {
+        // ...
+        WCA_ACCENT_POLICY = 19
+        // ...
+    }
+
+    /// <summary>
+    /// POINTAPI
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        /// <summary>
+        /// x coordinate of point.
+        /// </summary>
+        public int x;
+        /// <summary>
+        /// y coordinate of point.
+        /// </summary>
+        public int y;
+
+        /// <summary>
+        /// Construct a point of coordinates (x,y).
+        /// </summary>
+        public POINT(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MINMAXINFO
+    {
+        public POINT ptReserved;
+        public POINT ptMaxSize;
+        public POINT ptMaxPosition;
+        public POINT ptMinTrackSize;
+        public POINT ptMaxTrackSize;
+    };
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class MONITORINFO
+    {
+        /// <summary>
+        /// </summary>            
+        public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+
+        /// <summary>
+        /// </summary>            
+        public RECT rcMonitor = new RECT();
+
+        /// <summary>
+        /// </summary>            
+        public RECT rcWork = new RECT();
+
+        /// <summary>
+        /// </summary>            
+        public int dwFlags = 0;
+    }
+
+    /// <summary> Win32 </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    public struct RECT
+    {
+        /// <summary> Win32 </summary>
+        public int left;
+        /// <summary> Win32 </summary>
+        public int top;
+        /// <summary> Win32 </summary>
+        public int right;
+        /// <summary> Win32 </summary>
+        public int bottom;
+
+        /// <summary> Win32 </summary>
+        public static readonly RECT Empty = new RECT();
+
+        /// <summary> Win32 </summary>
+        public int Width
+        {
+            get { return Math.Abs(right - left); }  // Abs needed for BIDI OS
+        }
+        /// <summary> Win32 </summary>
+        public int Height
+        {
+            get { return bottom - top; }
+        }
+
+        /// <summary> Win32 </summary>
+        public RECT(int left, int top, int right, int bottom)
+        {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+
+
+        /// <summary> Win32 </summary>
+        public RECT(RECT rcSrc)
+        {
+            this.left = rcSrc.left;
+            this.top = rcSrc.top;
+            this.right = rcSrc.right;
+            this.bottom = rcSrc.bottom;
+        }
+
+        /// <summary> Win32 </summary>
+        public bool IsEmpty
+        {
+            get
+            {
+                // BUGBUG : On Bidi OS (hebrew arabic) left > right
+                return left >= right || top >= bottom;
+            }
+        }
+        /// <summary> Return a user friendly representation of this struct </summary>
+        public override string ToString()
+        {
+            if (this == RECT.Empty) { return "RECT {Empty}"; }
+            return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
+        }
+
+        /// <summary> Determine if 2 RECT are equal (deep compare) </summary>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Rect)) { return false; }
+            return (this == (RECT)obj);
+        }
+
+        /// <summary>Return the HashCode for this struct (not garanteed to be unique)</summary>
+        public override int GetHashCode()
+        {
+            return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
+        }
+
+
+        /// <summary> Determine if 2 RECT are equal (deep compare)</summary>
+        public static bool operator ==(RECT rect1, RECT rect2)
+        {
+            return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
+        }
+
+        /// <summary> Determine if 2 RECT are different(deep compare)</summary>
+        public static bool operator !=(RECT rect1, RECT rect2)
+        {
+            return !(rect1 == rect2);
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        [DllImport("User32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+
+        private WindowState previousState;
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            Loaded += EnableBlur;
+            SourceInitialized += FixMaximizeBehavior;
+            StateChanged += OnStateChanged;
+        }
+
+        private void StateChangedFromMinToMax(object sender, EventArgs e)
+        {
+            StateChanged -= StateChangedFromMinToMax;
+            WindowState = WindowState.Normal;
+            WindowState = WindowState.Maximized;
+        }
+
+        private void OnStateChanged(object sender, EventArgs e)
+        {
+            if (previousState == WindowState.Maximized && WindowState == WindowState.Minimized)
+                StateChanged += StateChangedFromMinToMax;
+
+            previousState = WindowState;
+        }
+
+        private void EnableBlur(object sender, RoutedEventArgs args)
+        {
+            var accent = new AccentPolicy();
+            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+            var accentStructSize = Marshal.SizeOf(accent);
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            var handler= (new WindowInteropHelper(this)).Handle;
+            SetWindowCompositionAttribute(handler, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+        private void FixMaximizeBehavior(object sender, EventArgs e)
+        {
+            var handler = (new WindowInteropHelper(this)).Handle;
+            HwndSource.FromHwnd(handler).AddHook(new HwndSourceHook(WindowProc));
+        }
+
+        private static IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case 0x0024:/* WM_GETMINMAXINFO */
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    handled = true;
+                    break;
+            }
+            return (IntPtr)0;
+        }
+
+
+        private static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != IntPtr.Zero)
+            {
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
         }
     }
 }
